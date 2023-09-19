94632469a6ce3a1ed7c6637860a2229379d413fe
lee14k <kmkovach@mtu.edu>
2023-09-19 14:00:12 -0400

    copy changes and fleshing out events page

--- components/Midgrid.jsx
+++ components/Midgrid.jsx
@@ -8,13 +8,19 @@ function Midgrid() {
 
         <div className="one">
           <div className="oneframe">
-            <h1 className="header1">Your Header 1</h1>
+            <h1 className="header1">Local Books</h1>
+            <p>
+              As a small business in the heart of the Upper Peninsula of Michiga, we are proud to carry and support the areas talented and diverse authors.
+            </p>
+            <button>Learn more</button>
           </div>
         </div>
 
         <div className="two">
           <div className="oneframe">
-            <h1 className="header2">Your Header 2</h1>
+            <h1 className="header2">Fresh Squeezed Juice</h1>
+            <p> Looking for some delicious orange juice? Look no further than the Falling Rock Cafe. Come try a delicious glass of our freshly squeezed juice</p>
+            <button>Learn more</button>
           </div>
         </div>
                   <div className="gridItemtwo"></div>
--- pages/events.js
+++ pages/events.js
@@ -31,11 +31,12 @@ export default function Events() {
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
-              <h2 className="text-base font-semibold leading-7 text-indigo-600"></h2>
-              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</p>
+              <h2 className="text-base font-semibold leading-7 text-indigo-600">Music & Talent</h2>
+              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Events</p>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
-                iste dolor cupiditate blanditiis ratione.
+            Live music and community events have become a regular occurrence at the Falling Rock in Munising, Michigan. Special events for children, book lovers, and artists are also regularly featured. 
+
+
               </p>
               <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                 {features.map((feature) => (
@@ -51,13 +52,22 @@ export default function Events() {
             </div>
           </div>
           <img
-            src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+            src="./events.jpg"
             alt="Product screenshot"
             className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
             width={2432}
             height={1442}
           />
         </div>
+      </div>
+        <div className="overflow-hidden bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+       <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-1">
+      <h3 className="text-6xl font-semibold  text-indigo-600 ">Interested in performing at the Falling Rock Cafe?</h3>
+      <p>Click below to fill out a form, and we will reach out with further information</p>
+      <button>Potential performers</button>
+      </div>
+      </div>
       </div>
     </div>
     </>
